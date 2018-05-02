d0a5eaa179f8d26cefb9ab717531538380609754
박경욱 <kwpark@kwpark-MacBook-pro.local>
2018-05-02 23:54:58 +0900

    Fixed font order in custom.css and removed unnecessary jsp files and settings

--- src/main/java/kr/co/redbrush/webapp/admin/controller/IndexController.java
+++ src/main/java/kr/co/redbrush/webapp/admin/controller/IndexController.java
@@ -11,7 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class IndexController {
 
-    @GetMapping("/")
+    @GetMapping({"/", "/index"})
     public String index(Map<String, Object> model) {
         LOGGER.debug("Index called model : {}", model);
 
