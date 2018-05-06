a79428e87dfae20452cb3c1ff9d736d725333829
박경욱 <kwpark@kwpark-MacBook-pro.local>
2018-05-06 17:39:33 +0900

    Fix to apply Mustache template layout

--- src/main/java/kr/co/redbrush/webapp/admin/controller/LayoutAdvice.java
+++ src/main/java/kr/co/redbrush/webapp/admin/controller/LayoutAdvice.java
@@ -2,16 +2,27 @@ package kr.co.redbrush.webapp.admin.controller;
 
 import com.samskivert.mustache.Mustache;
 import kr.co.redbrush.webapp.admin.view.Layout;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import java.util.Map;
+
 @ControllerAdvice
 public class LayoutAdvice {
-    @ModelAttribute("header")
-    public Mustache.Lambda defaults(@ModelAttribute Layout layout) {
-        return (frag, out) -> {
-            layout.setHeader(frag.execute());
-            layout.setHeader("Test");
-        };
+    @Autowired
+    private Mustache.Compiler compiler;
+
+    // TOOD : Implement layout
+    /*
+    @ModelAttribute("layout")
+    public Mustache.Lambda layout(Map<String, Object> model) {
+        return new Layout(compiler);
+    }
+    */
+
+    @ModelAttribute("layout")
+    public Mustache.Lambda layout() {
+        return new Layout();
     }
 }
--- src/main/java/kr/co/redbrush/webapp/admin/view/Layout.java
+++ src/main/java/kr/co/redbrush/webapp/admin/view/Layout.java
@@ -13,8 +13,25 @@ public class Layout implements Mustache.Lambda {
     private String content;
     private String scripts;
 
+    private Mustache.Compiler compiler;
+
+    public Layout() {
+
+    }
+
+    public Layout(Mustache.Compiler compiler) {
+        this.compiler = compiler;
+    }
+
     @Override
     public void execute(Template.Fragment frag, Writer out) throws IOException {
+        header = frag.execute();
+        content = frag.execute();
+        scripts = frag.execute();
 
+        // TODO : Implement execute
+        /*
+        compiler.compile("{{>layout}}").execute(frag.context(), out);
+        */
     }
 }
