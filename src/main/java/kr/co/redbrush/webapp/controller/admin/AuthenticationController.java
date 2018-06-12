09c7ce4aab885f5df8104fc3fefbe7b9dc9ad84c
박경욱 <sirius00@paran.com>
2018-06-12 22:51:33 +0900

    Added signup view and test case in the AuthenticationController

--- src/main/java/kr/co/redbrush/webapp/controller/admin/AuthenticationController.java
+++ src/main/java/kr/co/redbrush/webapp/controller/admin/AuthenticationController.java
@@ -1,7 +1,9 @@
 package kr.co.redbrush.webapp.controller.admin;
 
+import kr.co.redbrush.webapp.service.AccountService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.BooleanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,12 +15,21 @@ import java.util.Map;
 @Slf4j
 public class AuthenticationController {
     public static final String SPRING_SECURITY_LAST_EXCEPTION = "SPRING_SECURITY_LAST_EXCEPTION";
+    public static final String VIEW_LOGIN = "login";
+    public static final String VIEW_SIGNUP = "signup";
+
+    @Autowired
+    private AccountService accountService;
 
     @GetMapping("/login/form")
     public String loginForm(HttpServletRequest request, Map<String, Object> model, String error) {
         setErrorCondition(request, model, error);
 
-        return "login";
+        if (accountService.getCount() == 0) {
+            return VIEW_SIGNUP;
+        } else {
+            return VIEW_LOGIN;
+        }
     }
 
     private void setErrorCondition(HttpServletRequest request, Map<String, Object> model, String error) {
