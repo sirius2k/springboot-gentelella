a0d6264149d2fd61a5e734593f505b6a6a1cf39e
박경욱 <sirius00@paran.com>
2018-07-06 23:16:03 +0900

    Fixed user details implementation

--- src/main/java/kr/co/redbrush/webapp/config/SecurityConfig.java
+++ src/main/java/kr/co/redbrush/webapp/config/SecurityConfig.java
@@ -77,6 +77,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .csrf()
                         .ignoringAntMatchers("/h2/**")
                 .and()
-                    .logout();
+                    .logout()
+                        .logoutUrl("/logout");
     }
 }
