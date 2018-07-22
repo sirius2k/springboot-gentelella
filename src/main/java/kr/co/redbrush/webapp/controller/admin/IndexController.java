59fdb7b8650bb93468a4ebbf84374bdfc3102481
박경욱 <sirius00@paran.com>
2018-07-22 11:17:58 +0900

    Added dashboard1,2,3

--- src/main/java/kr/co/redbrush/webapp/controller/admin/IndexController.java
+++ src/main/java/kr/co/redbrush/webapp/controller/admin/IndexController.java
@@ -3,6 +3,7 @@ package kr.co.redbrush.webapp.controller.admin;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Map;
 
@@ -10,10 +11,17 @@ import java.util.Map;
 @Slf4j
 public class IndexController {
 
-    @GetMapping({"/", "/index", "/index/0"})
+    @GetMapping({"/", "/index"})
     public String index(Map<String, Object> model) {
-        model.put("title", "Springboot Web Service");
+        model.put("title", "Dashboard 1");
 
-        return "index";
+        return "index1";
+    }
+
+    @GetMapping("/index/{indexNum}")
+    public String index2(Map<String, Object> model, @PathVariable("indexNum") int indexNum) {
+        model.put("title", "Dashboard " + indexNum);
+
+        return "index" + indexNum;
     }
 }
