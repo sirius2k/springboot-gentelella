f418a5e56b90a68c0037eee57c22ee9801a52e86
박경욱 <sirius00@paran.com>
2018-08-20 21:55:48 +0900

    Added Configuration of Handlebars SpringScurityHelper

--- src/main/java/kr/co/redbrush/webapp/config/WebMVCConfig.java
+++ src/main/java/kr/co/redbrush/webapp/config/WebMVCConfig.java
@@ -1,12 +1,19 @@
 package kr.co.redbrush.webapp.config;
 
+import com.github.jknack.handlebars.springmvc.HandlebarsViewResolver;
+import kr.co.redbrush.webapp.handlebars.SpringSecurityHelper;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import javax.annotation.PostConstruct;
+
 @Configuration
+@ConditionalOnClass(HandlebarsViewResolver.class)
 public class WebMVCConfig {
     @Bean
     public RestTemplate getRestTemplate() {
@@ -21,4 +28,18 @@ public class WebMVCConfig {
         return modelMapper;
     }
 
+    @Configuration
+    @ConditionalOnClass(SpringSecurityHelper.class)
+    static class SpringSecurityHelperAutoConfiguration {
+        @Autowired
+        private HandlebarsViewResolver handlebarsViewResolver;
+
+        @Autowired
+        private SpringSecurityHelper springSecurityHelper;
+
+        @PostConstruct
+        public void registerHelper() {
+            handlebarsViewResolver.registerHelper(SpringSecurityHelper.NAME, springSecurityHelper);
+        }
+    }
 }
